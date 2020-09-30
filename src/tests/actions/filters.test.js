cd8694ee2329b034835c30936dc06874628ea7bf
Yama <yama_1800@yahoo.com>
2020-09-29 23:40:50 -0700

    set prod

--- src/tests/actions/filters.test.js
+++ src/tests/actions/filters.test.js
@@ -1,46 +1,49 @@
-import moment from "moment";
-import { setEndDate, setStartDate, setTextFilter, sortByAmount, sortByDate } from "../../actions/filters";
+import moment from 'moment';
+import {
+  setStartDate,
+  setEndDate,
+  setTextFilter,
+  sortByAmount,
+  sortByDate
+} from '../../actions/filters';
 
-test("should test start date action", () => {
+test('should generate set start date action object', () => {
   const action = setStartDate(moment(0));
   expect(action).toEqual({
-    type: "SET_START_DATE",
-    startDate: moment(0),
+    type: 'SET_START_DATE',
+    startDate: moment(0)
   });
 });
 
-test("should test end date action", () => {
+test('should generate set end date aciton object', () => {
   const action = setEndDate(moment(0));
   expect(action).toEqual({
-    type: "SET_END_DATE",
-    endDate: moment(0),
+    type: 'SET_END_DATE',
+    endDate: moment(0)
   });
 });
 
-test("should test filter by text", () => {
-  const action = setTextFilter("good");
+test('should generate set text filter object with text value', () => {
+  const text = 'Something in';
+  const action = setTextFilter(text);
   expect(action).toEqual({
-    type: "SET_TEXT_FILTER",
-    text: "good",
+    type: 'SET_TEXT_FILTER',
+    text
   });
 });
 
-test("should test filter text by default", () => {
+test('should generate set text filter object with default', () => {
   const action = setTextFilter();
   expect(action).toEqual({
-    type: "SET_TEXT_FILTER",
+    type: 'SET_TEXT_FILTER',
     text: ''
   });
 });
 
-test('should test sort bay amount', ()=> {
-    const action = sortByAmount()
-    expect(action).toEqual({
-        type: 'SORT_BY_AMOUNT'
+test('should generate action object for sort by date', () => {
+  expect(sortByDate()).toEqual({ type: 'SORT_BY_DATE' });
+});
 
-    })
-})
-
-test('should tst sort by date', ()=> {
-    expect(sortByDate()).toEqual({type: 'SORT_BY_DATE'})
-})
+test('should generate action object for sort by amount', () => {
+  expect(sortByAmount()).toEqual({ type: 'SORT_BY_AMOUNT' });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = (env) => {
   const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
 
   return {
     entry: './src/app.js',
     output: {
-      path: path.join(__dirname, 'public'),
+      path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js'
     },
     module: {
@@ -16,17 +18,32 @@ module.exports = (env) => {
         exclude: /node_modules/
       }, {
         test: /\.s?css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
+        use: CSSExtract.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        })
       }]
     },
-    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    plugins: [
+      CSSExtract
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
-      historyApiFallback: true
+      historyApiFallback: true,
+      publicPath: '/dist/'
     }
   };
 };
